ea0ef3779ef9d4d54b327ce3a447024ea2816b00
houxinlin <2606710413@qq.com>
2024-03-04 21:29:42 +0800

    兼容非SpringBoot程序

--- src/main/java/com/hxl/plugin/scheduledinvokestarter/components/spring/controller/EnabledSpringMvcRequestMapping.java
+++ src/main/java/com/hxl/plugin/scheduledinvokestarter/components/spring/controller/EnabledSpringMvcRequestMapping.java
@@ -5,14 +5,24 @@ import com.hxl.plugin.scheduledinvokestarter.components.ComponentSupport;
 import com.hxl.plugin.scheduledinvokestarter.components.SpringBootStartInfo;
 import com.hxl.plugin.scheduledinvokestarter.json.JsonMapper;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.WebApplicationContext;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 
 public class EnabledSpringMvcRequestMapping implements ComponentSupport {
-    private static final String className = "com.hxl.plugin.scheduledinvokestarter.components.spring.controller.SpringRequestMappingComponent";
 
     @Override
     public boolean canSupport(ApplicationContext applicationContext) {
-        return applicationContext instanceof WebApplicationContext;
+        try {
+            Class.forName("org.springframework.web.context.WebApplicationContext");
+            Class<?> aClass = Class.forName("com.hxl.plugin.scheduledinvokestarter.components.spring.controller.SpringWebApplicationCheck");
+            MethodType methodType = MethodType.methodType(boolean.class, ApplicationContext.class);
+            MethodHandle test = MethodHandles.lookup().findStatic(aClass, "check", methodType);
+            return (boolean) test.invoke(applicationContext);
+        } catch (Throwable e) {
+            return false;
+        }
     }
 
     @Override
